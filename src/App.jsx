85990d4651515c335bdfa3603373b1063fa017c5
Rabeeh mv <rabeehmv333@gmail.com>
2025-01-04 09:18:48 -0800

    thierd

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Admin from './operations/admin'
+import Addstd from './Addstd/Addstd'
 function App() {
 
   return (
 <>
 <Admin/>
+{/* <Addstd/> */}
 </>
   )
 }
--- src/operations/admin.jsx
+++ src/operations/admin.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Addstd from '../Addstd/Addstd'
+import Hospitals from '../Hospital/hospitals'
+import Payment from '../payment/payment'
+import AddUpdates from '../Updates/AddUpdates'
 
 function admin() {
+  const [activeCombonet, setactiveCombonet]= useState('student')
+  const list = (data)=>{
+    setactiveCombonet(data)
+  }
   return (
     <div>
-      admin
+      <h2>admin panel</h2>
+      <div>
+        <ul>
+          <li onClick={()=>list('student')}>add student</li>
+          <li onClick={()=>list('hospital')}>add hospital</li>
+          <li onClick={()=>list('payment')}>payment</li>
+          <li onClick={()=>list('Updates')}>add Updates</li>
+        </ul>
+      </div>
+      {activeCombonet === 'student' && <Addstd/>}
+      {activeCombonet === 'hospital' && <Hospitals/>}
+      {activeCombonet === 'payment' && <Payment/>}
+      {activeCombonet === 'Updates' && <AddUpdates/>}
     </div>
   )
 }
